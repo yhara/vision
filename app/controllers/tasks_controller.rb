af3b468eb864b12703f3e376a4d3b71ca4845633
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-06 22:58:58 +0900

    Show undone tasks only

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.undone
   end
 
   # GET /tasks/1
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   validates :title, {presence: true}
+
+  scope :done, ->{ where(done: true) }
+  scope :undone, ->{ where(done: false) }
 end
