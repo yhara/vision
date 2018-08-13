ffed843fed9bf6c7343a6b1c0aaa9ab36342e2a3
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-13 19:52:49 +0900

    feat: DnD to change project

--- app/assets/javascripts/ovto_app/actions/dnd_actions.js.rb
+++ app/assets/javascripts/ovto_app/actions/dnd_actions.js.rb
@@ -23,12 +23,26 @@ class MyApp < Ovto::App
 
       def drag_drop(state:)
         task = state.tasks.find{|t| t.id == state.drag_info.task_id}
-        date = state.drag_info.drop_target.key
         updated_task = task.dup
-        if date && date != task.due_date
-          new_date = (date == DATE_UNSORTED ? nil : date)
-          updated_task = task.merge(due_date: new_date)
-          actions.request_update_task(task: task, updates: {due_date: new_date})
+        drop_target = state.drag_info.drop_target
+        updates = nil
+        case drop_target.type
+        when "due_date"
+          date = drop_target.key
+          if date != task.due_date
+            new_date = (date == DATE_UNSORTED ? nil : date)
+            updates = {due_date: new_date}
+          end
+        when "project"
+          project_id = drop_target.key
+          if project_id != task.project_id
+            updates = {project_id: project_id}
+          end
+        else raise
+        end
+        if updates
+          updated_task = task.merge(**updates)
+          actions.request_update_task(task: task, updates: updates)
         end
         return {
           drag_info: state.drag_info.merge(task_id: nil, drop_target: DropTarget.new, dragover_occurred: false),
--- app/assets/javascripts/ovto_app/components/sidebar.js.rb
+++ app/assets/javascripts/ovto_app/components/sidebar.js.rb
@@ -6,12 +6,33 @@ class MyApp < Ovto::App
       def render(state:)
         o '.Sidebar' do
           o 'h2', 'Projects'
-          o 'ul' do
-            state.projects.each do |project|
-              o 'li', project.title
-            end
+          o ProjectList, projects: state.projects
+        end
+      end
+    end
+
+    class ProjectList < Ovto::Component
+      def render(projects:)
+        o 'ul.ProjectList' do
+          projects.each do |project|
+            o ProjectListItem, project: project
           end
         end
+      end
+    end
+
+    class ProjectListItem < Ovto::Component
+      def render(state:, project:)
+        is_hovered = state.drag_info.drop_target.type == "project" &&
+                     state.drag_info.drop_target.key == project.id
+        drop_target = DropTarget.new(type: "project", key: project.id)
+        o 'li.ProjectListItem', {
+          ondragenter: ->{ actions.drag_enter(drop_target: drop_target) },
+          ondragover: ->(e){ actions.drag_over(); e.preventDefault() },
+          ondragleave: ->{ actions.drag_leave() },
+          ondrop: ->(e){ actions.drag_drop() },
+          class: (is_hovered && 'hover')
+        }, project.title
       end
     end
   end
