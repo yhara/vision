b39a64f6d66793f53e2fca7ca0a887ca611f9978
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-13 20:31:14 +0900

    refactor: fix indent

--- app/assets/javascripts/ovto_app/view.js.rb
+++ app/assets/javascripts/ovto_app/view.js.rb
@@ -126,44 +126,44 @@ class MyApp < Ovto::App
         id_due_date = "TaskDetails-due-date" 
         id_project = "TaskDetails-project"
         o '.TaskDetailsContainer' do
-        o '.TaskDetails' do
-          o 'div' do
-            o 'label', {for: id_title}, 'Title:'
-            o 'input', id: id_title, type: 'text', value: task.title
-          end
-          o 'div' do
-            o 'label', {for: id_due_date}, 'Due date:'
-            o 'input', id: id_due_date, type: 'date', value: task.due_date
-          end
-          o 'div' do
-            o 'label', {for: id_project}, 'Project:'
-            o 'select', id: id_project do
-              o 'option', {value: ""}, "---"
-              state.projects.each do |project|
-                o 'option', {
-                  value: project.id,
-                  selected: task.project_id == project.id
-                }, project.title 
+          o '.TaskDetails' do
+            o 'div' do
+              o 'label', {for: id_title}, 'Title:'
+              o 'input', id: id_title, type: 'text', value: task.title
+            end
+            o 'div' do
+              o 'label', {for: id_due_date}, 'Due date:'
+              o 'input', id: id_due_date, type: 'date', value: task.due_date
+            end
+            o 'div' do
+              o 'label', {for: id_project}, 'Project:'
+              o 'select', id: id_project do
+                o 'option', {value: ""}, "---"
+                state.projects.each do |project|
+                  o 'option', {
+                    value: project.id,
+                    selected: task.project_id == project.id
+                  }, project.title 
+                end
               end
             end
-          end
-          o 'div' do
-            o 'input.save-button', type: 'button', value: 'Save', onclick: ->{
-              actions.request_update_task(task: task, updates: {
-                title: `document.querySelector('#'+id_title).value`,
-                due_date: `document.querySelector('#'+id_due_date).value`,
-                project_id: `document.querySelector('#'+id_project).value`.to_i,
-              })
-              actions.hide_task_details()
-            }
-            o 'a', {
-              href: '#', onclick: ->(e){
-                e.preventDefault()
+            o 'div' do
+              o 'input.save-button', type: 'button', value: 'Save', onclick: ->{
+                actions.request_update_task(task: task, updates: {
+                  title: `document.querySelector('#'+id_title).value`,
+                  due_date: `document.querySelector('#'+id_due_date).value`,
+                  project_id: `document.querySelector('#'+id_project).value`.to_i,
+                })
                 actions.hide_task_details()
               }
-            }, 'Close'
+              o 'a', {
+                href: '#', onclick: ->(e){
+                  e.preventDefault()
+                  actions.hide_task_details()
+                }
+              }, 'Close'
+            end
           end
-        end
         end
       end
     end
