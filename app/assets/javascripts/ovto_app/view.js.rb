e28b074e3ca69f941733d71806088673958b7225
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-24 01:40:28 +0900

    refactor: Rename file

--- app/assets/javascripts/ovto_app/view.js.rb
+++ app/assets/javascripts/ovto_app/view.js.rb
@@ -1,5 +1,5 @@
 require 'ovto'
-require 'ovto_app/components/current_task_list'
+require 'ovto_app/components/main_content'
 require 'ovto_app/components/sidebar'
 require 'ovto_app/components/task_details'
 
