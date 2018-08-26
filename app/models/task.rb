fba772379a51689cde9a1153aa3b43c9b6296e0b
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-27 02:39:15 +0900

    feat: Task#create_next_task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,61 @@
 class Task < ApplicationRecord
   belongs_to :project, optional: true
 
+  enum interval_type: {
+    n_days: 0,
+    day_of_week: 1,
+    day_of_month: 2,
+    day_of_year: 3,
+  }
+
   validates :title, {presence: true}
+  validates :interval_value, presence: true, if: ->{ interval_type != nil }
+  validates :interval_value, numericality: { greater_than_or_equal_to: 1 }, if: ->{ interval_type == "n_days" }
+  validates :interval_value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 6 }, if: ->{ interval_type == "day_of_week" }
+  validates :interval_value, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }, if: ->{ interval_type == "day_of_month" }
+  validates :interval_value, numericality: { greater_than_or_equal_to: 101, less_than_or_equal_to: 1231 }, if: ->{ interval_type == "day_of_year" }
+
+  after_update :create_next_task
 
   scope :done, ->{ where(done: true) }
   scope :undone, ->{ where(done: false) }
+
+  private
+
+  # Create next task if interval_type is set
+  def create_next_task
+    return if !self.done || self.interval_type.nil?
+    Task.create!(title: self.title,
+                 done: false,
+                 due_date: next_due_date(Date.today),
+                 project_id: self.project_id,
+                 interval_type: self.interval_type,
+                 interval_value: self.interval_value)
+  end
+
+  def next_due_date(today)
+    case interval_type
+    when "n_days"
+      return today + interval_value
+    when "day_of_week"
+      dw = %i(sunday monday tuesday wednesday thursday friday saturday)[interval_value]
+      return today.next_occurring(dw)
+    when "day_of_month"
+      the_day = Date.new(today.year, today.month, interval_value)
+      if the_day > today
+        return the_day
+      else
+        return the_day.next_month
+      end
+    when "day_of_year"
+      the_day = Date.new(today.year, interval_value / 100, interval_value % 100)
+      if the_day > today
+        return the_day
+      else
+        return the_day.next_year
+      end
+    else
+      raise "invalid interval_type: #{interval_type.inspect}"
+    end
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'interval' do
+    should 'everyday(=1 day)' do
+      task = FactoryBot.create(:task,
+                               interval_type: :n_days, 
+                               interval_value: 1)
+      travel_to("2001-01-01")
+      task.update!(done: true)
+      assert_equal Date.new(2001, 1, 2), Task.last.due_date
+    end
+
+    should 'every 7 days' do
+      task = FactoryBot.create(:task,
+                               interval_type: :n_days, 
+                               interval_value: 7)
+      travel_to("2001-01-01")
+      task.update!(done: true)
+      assert_equal Date.new(2001, 1, 8), Task.last.due_date
+    end
+
+    should 'every sunday' do
+      task = FactoryBot.create(:task,
+                               interval_type: :day_of_week, 
+                               interval_value: 0)
+      travel_to("2001-01-01")  # Monday
+      task.update!(done: true)
+      assert_equal Date.new(2001, 1, 7), Task.last.due_date
+    end
+
+    should 'every 10th' do
+      task = FactoryBot.create(:task,
+                               interval_type: :day_of_month, 
+                               interval_value: 10)
+      travel_to("2001-01-01")  # Monday
+      task.update!(done: true)
+      assert_equal Date.new(2001, 1, 10), Task.last.due_date
+    end
+
+    should 'every 1st' do
+      task = FactoryBot.create(:task,
+                               interval_type: :day_of_month, 
+                               interval_value: 1)
+      travel_to("2001-01-01")  # Monday
+      task.update!(done: true)
+      assert_equal Date.new(2001, 2, 1), Task.last.due_date
+    end
+
+    should 'every May 21st' do
+      task = FactoryBot.create(:task,
+                               interval_type: :day_of_year, 
+                               interval_value: 521)
+      travel_to("2001-01-01")  # Monday
+      task.update!(done: true)
+      assert_equal Date.new(2001, 5, 21), Task.last.due_date
+    end
+  end
 end
