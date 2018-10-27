f050cdbb8caab62aa4b94f1f669e7c4934cb35f2
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-10-27 16:48:07 +0900

    refactor: Remove patch for old Opal

--- app/assets/javascripts/ovto_app/index.js.rb
+++ app/assets/javascripts/ovto_app/index.js.rb
@@ -4,18 +4,6 @@ require 'json'
 require 'set'
 require 'date'
 
-# Patch to fix Date#== for Opal < 0.11
-class Date
-  def ==(other)
-    return false unless Date === other
-    %x{
-      var a = #@date, b = other.date;
-      return (a.getFullYear() === b.getFullYear() && a.getMonth() === b.getMonth() && a.getDate() === b.getDate());
-    }
-  end
-  alias eql? ==
-end
-
 class MyApp < Ovto::App
   # Dummy date for unscheduled tasks
   DATE_UNSORTED = Date.new(2000, 1, 1)
