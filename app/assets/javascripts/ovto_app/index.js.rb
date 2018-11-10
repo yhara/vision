a52b392a7edb70e40dee70be686ea878333f6163
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-11-10 22:19:04 +0900

    chores: Follow api change of Ovto

--- app/assets/javascripts/ovto_app/index.js.rb
+++ app/assets/javascripts/ovto_app/index.js.rb
@@ -8,10 +8,8 @@ class MyApp < Ovto::App
   # Dummy date for unscheduled tasks
   DATE_UNSORTED = Date.new(2000, 1, 1)
 
-  def self.start
-    app = MyApp.new
-    app.run(id: 'ovto-view')
-    app.actions.get_tasks
-    app.actions.get_projects
+  def setup
+    actions.get_tasks
+    actions.get_projects
   end
 end
